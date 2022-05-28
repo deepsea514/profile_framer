03ec7a494cc532c0d2aa3221871e4bd9dbb45062
Black Storm <nightyellowstormy@gmail.com>
2022-05-28 22:09:11 +0900

    make profile and background change

--- scripts/frame_generator.js
+++ scripts/frame_generator.js
@@ -1,36 +1,64 @@
 class FrameGenerator {
-    constructor(canvas_id) {
+    constructor(canvas_id, profileImage, backgroundImage) {
         this.canvas_id = canvas_id;
         this.frameSize = {
             width: 800,
             height: 600
         }
         this.canvas = new fabric.Canvas(canvas_id, {
-            backgroundColor: '#000',
             width: this.frameSize.width,
-            height: this.frameSize.height
+            height: this.frameSize.height,
         });
-        this.backgroundImage = null;
-        this.profileImage = null;
+
+        this.backgroundImage = backgroundImage;
+        this.profileImage = profileImage;
+
+        this.onBackgroundImageChanged(backgroundImage);
+        this.onProfileImageChanged(profileImage);
     }
-
-    onFrameSizeChanged(width, height) {
-
+    // To Save Image as Image
+    onSaveAsImage() {
+        return this.canvas.toDataURL({
+            format: 'jpg',
+            quality: 1.0
+        });
     }
-
+    // When Background Image Changed.
     onBackgroundImageChanged(backgroundImage) {
-
+        if (backgroundImage) {
+            const canvas = this.canvas;
+            // Reset Canvas size
+            canvas.setWidth(backgroundImage.frameWidth);
+            canvas.setHeight(backgroundImage.frameHeight);
+            canvas.renderAll();
+            // Rendering new Background Image
+            fabric.Image.fromURL(backgroundImage.src, function (img) {
+                canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
+                    scaleX: backgroundImage.frameWidth / img.width,
+                    scaleY: backgroundImage.frameHeight / img.height
+                });
+            });
+        }
     }
-
+    // When Profile Image Changed.
     onProfileImageChanged(profileImage) {
-
-    }
-
-    onProfileImageSizeChanged(width, height) {
-
-    }
-
-    onProfileImagePositionChanged(left, top) {
-
+        const canvas = this.canvas;
+        // Removing previous image
+        const previousObjects = canvas.getObjects();
+        if (previousObjects) {
+            previousObjects.map(object => canvas.remove(object));
+        }
+        // Generate new image
+        fabric.Image.fromURL(profileImage.src, function (image) {
+            const img = image.set({
+                left: profileImage.left,
+                top: profileImage.top,
+                width: image.width,
+                height: image.height,
+                scaleX: profileImage.width / image.width,
+                scaleY: profileImage.height / image.height,
+            });
+            canvas.add(img);
+        });
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,175 @@
-$(document).ready(function() {
-    const frameGenerator = new FrameGenerator('canvas');
+$(document).ready(function () {
+    /* Initialize the canvas */
+    const backgroundImage = getBackgroundImage();
+    const profileImage = getProfileImage();
+    const frameGenerator = new FrameGenerator('canvas', profileImage, backgroundImage);
+
+    /* Global Functions */
+    function getFrameSize() {
+        const width = $('#frame-width-input').val();
+        const height = $('#frame-height-input').val();
+        return {
+            width: Number(width),
+            height: Number(height)
+        }
+    }
+
+    function getProfileSize() {
+        const width = $('#profile-width-input').val();
+        const height = $('#profile-height-input').val();
+        return {
+            width: Number(width),
+            height: Number(height)
+        }
+    }
+
+    function getProfilePosition() {
+        const left = $('#profile-left-input').val();
+        const top = $('#profile-top-input').val();
+        return {
+            left: Number(left),
+            top: Number(top)
+        }
+    }
+
+    function getProfileImage() {
+        const images = $('.profile-image.selected img');
+        const { width, height } = getProfileSize();
+        const { left, top } = getProfilePosition();
+        if (images.length) {
+            return {
+                src: images[0].src,
+                width: width,
+                height: height / images[0].naturalWidth * images[0].naturalHeight,
+                left: left,
+                top: top,
+            };
+        }
+        return null;
+    }
+
+    function getBackgroundImage() {
+        const { width, height } = getFrameSize();
+        const images = $('.background-image.selected img');
+        if (images.length) {
+            return {
+                src: images[0].src,
+                width: images[0].naturalWidth,
+                height: images[0].naturalHeight,
+                frameWidth: width,
+                frameHeight: height,
+            };
+        }
+        return null;
+    }
+
+    function addProfileImage(image) {
+        const imageString =
+            `<div class="profile-image">
+                <img src="${image}" />
+            </div>`;
+        const imgElement = $.parseHTML(imageString);
+        $('.profile-images-gallery-container').append(imgElement);
+        // On Profile Image Clicked
+        $('.profile-image img').click(function (event) {
+            // Remove previously selected profie image
+            $('.profile-image.selected').removeClass('selected');
+            // Newly set profile image
+            $(event.target).parent().addClass('selected');
+            frameGenerator.onProfileImageChanged(getProfileImage());
+        })
+    }
+
+    function addBackgroundImage(image) {
+        const imageString =
+            `<div class="background-image">
+                <img src="${image}" />
+            </div>`;
+        const imgElement = $.parseHTML(imageString);
+        $('.background-images-gallery-container').append(imgElement);
+        // On Background Image Clicked
+        $('.background-image img').click(function (event) {
+            // Remove previously selected profie image
+            $('.background-image.selected').removeClass('selected');
+            // Newly set background image
+            $(event.target).parent().addClass('selected');
+            frameGenerator.onBackgroundImageChanged(getBackgroundImage());
+        })
+    }
+
+    /* Event Listeners */
+    // On Save Image
+    $('#save-image-button').click(function () {
+        this.href = frameGenerator.onSaveAsImage();
+        this.download = 'canvas.png'
+    })
+
+    // Add Profile Image Button Clicked
+    $('#profile-image-button').click(function () {
+        $('#profile-image-input').click();
+    })
+    // On Profile Image Uploaded
+    $('#profile-image-input').change(function () {
+        const files = this.files;
+
+        for (let i = 0; i < files.length; i++) {
+            const reader = new FileReader();
+            reader.onloadend = function () {
+                addProfileImage(reader.result);
+            }
+            reader.readAsDataURL(files[i]);
+        }
+    })
+    // On Profile Image Clicked
+    $('.profile-image img').click(function (event) {
+        // Remove previously selected profie image
+        $('.profile-image.selected').removeClass('selected');
+        // Newly set profile image
+        $(event.target).parent().addClass('selected');
+        frameGenerator.onProfileImageChanged(getProfileImage());
+    })
+
+    // Add Background Image Button Clicked
+    $('#background-image-button').click(function () {
+        $('#background-image-input').click();
+    })
+    // On Background Image Uploaded
+    $('#background-image-input').change(function () {
+        const files = this.files;
+
+        for (let i = 0; i < files.length; i++) {
+            const reader = new FileReader();
+            reader.onloadend = function () {
+                addBackgroundImage(reader.result);
+            }
+            reader.readAsDataURL(files[i]);
+        }
+    })
+
+    // On Background Image Clicked
+    $('.background-image img').click(function (event) {
+        // Remove previously selected background
+        $('.background-image.selected').removeClass('selected');
+        // Newly set background image
+        $(event.target).parent().addClass('selected');
+        frameGenerator.onBackgroundImageChanged(getBackgroundImage())
+    })
+
+    // On Frame Size changed
+    $('#frame-size-form').submit(function (event) {
+        event.preventDefault();
+        frameGenerator.onBackgroundImageChanged(getBackgroundImage())
+    })
+
+    // On Profile Size changed
+    $('#profile-size-form').submit(function (event) {
+        event.preventDefault();
+        frameGenerator.onProfileImageChanged(getProfileImage());
+    })
+
+    // On Profile Position changed
+    $('#profile-position-form').submit(function (event) {
+        event.preventDefault();
+        frameGenerator.onProfileImageChanged(getProfileImage());
+    })
 })
